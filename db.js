83a93d542982c43a2cfaf1cfd751111025119e51
Felix C. Stegerman <felixstegerman@noxqslabs.nl>
2012-03-15 14:24:30 +0100

    ~ db.js
    Signed-off-by: Felix C. Stegerman <felixstegerman@noxqslabs.nl>

--- db.js
+++ db.js
@@ -45,7 +45,7 @@ function db_with (name) {                                     //  {{{1
 //
 
 function db_seq (name) {                                      //  {{{1
-  return function (fs, f_error) {               
+  return function (fs, f_error) {
     var w = db_with (name);
     var f = function () {};
 
@@ -61,54 +61,6 @@ function db_seq (name) {                                      //  {{{1
 
 
 //
-//  Usage: db_def (name, version, d_name, size, tables[, fs]) -> none
-//
-//  Example:
-//    db_def (
-//      'Foo Bar Baz', '1.0', '...', 200000, {
-//        tbl_foo: [
-//          'foo TEXT'    , // ...
-//          'bar INTEGER' , // ...
-//        ], ...
-//      }, function () { ... }
-//    );
-//
-
-function db_def (name, version, d_name, size, tables, fs) {   //  {{{1
-  DB[name] = {
-    name:     name          ,
-    version:  version       ,
-    d_name:   d_name        ,
-    size:     size          ,
-    tables:   tables        ,
-    fields:   {}            ,
-    seq:      db_seq (name) ,
-  };
-
-  DB[name].seq (
-    [ function (tx) {
-        for (var k in tables) {
-          DB[name].fields[k] = tables[k].map (
-            function (x) { return x.split (/\s+/)[0]; }
-          );
-
-          sql = 'CREATE TABLE IF NOT EXISTS '
-              + k + ' (id INTEGER PRIMARY KEY, '
-              + tables[k].join (', ') + ' );';
-
-          if (DEBUG) { console.log (sql); }
-
-          tx.executeSql (sql);
-        }
-      }
-    ].concat (fs || []), db_error_cb
-  )
-
-  // ... TODO ...
-}                                                             //  }}}1
-
-
-//
 //  Usage: db_defns (name) -> none
 //
 
@@ -119,7 +71,7 @@ function db_defns (name) {                                    //  {{{1
     var t = tables[i];
     var T = t[0].toUpperCase () + t.substr (1);
 
-    var f_q = function (f, f_error, w, v) { 
+    var f_q = function (f, f_error, w, v) {
       return db_query (
         DB[name].table, DB[name].fields], f, f_error, w, v
       );
@@ -137,6 +89,67 @@ function db_defns (name) {                                    //  {{{1
   }
 }                                                             //  }}}1
 
+
+//
+//  Usage: db (name, version, d_name, size, tables[, f]) -> none
+//
+//  Example:
+//    db (
+//      'Foo Bar Baz', '1.0', '...', 200000, {
+//        tbl_foo: [
+//          'foo TEXT'    , // ...
+//          'bar INTEGER' , // ...
+//        ], ...
+//      }, function () { return [
+//        function (tx) { ... },
+//        function (tx) { ... },
+//      ]; }
+//    );
+//
+
+function db (name, version, d_name, size, tables, f) {        //  {{{1
+  var f_ = f == none ? function () { return []; } : f;
+
+  if (DB[name] != none) {
+    DB[name].seq (f_ (), db_error_cb);
+
+    return;                                                   //  !!!!
+  }
+
+  DB[name] = {
+    name:     name          ,
+    version:  version       ,
+    d_name:   d_name        ,
+    size:     size          ,
+    tables:   tables        ,
+    fields:   {}            ,
+    seq:      db_seq (name) ,
+  };
+
+  for (var k in tables) {
+    DB[name].fields[k] = tables[k].map (
+      function (x) { return x.split (/\s+/)[0]; }
+    );
+  }
+
+  db_defns (name);
+
+  DB[name].seq (
+    [ function (tx) {
+        for (var k in tables) {
+          sql = 'CREATE TABLE IF NOT EXISTS '
+              + k + ' (id INTEGER PRIMARY KEY, '
+              + tables[k].join (', ') + ' );';
+
+          if (DEBUG) { console.log (sql); }
+
+          tx.executeSql (sql);
+        }
+      }
+    ].concat (f_ ()), db_error_cb
+  )
+}                                                             //  }}}1
+
 // --
 
 //
@@ -144,14 +157,18 @@ function db_defns (name) {                                    //  {{{1
 //          -> none
 //
 //  Example:
-//    ...
+//    db (..., function () { return [
+//      ...
+//      DB.foo.qFoo (f, f_error),   // iterates: f (i, x)
+//      ...
+//    ]; });
 //
 
 function db_query (table, fields, f, f_error, w, vs) {        //  {{{1
   var vs_ = vs == none ? [] : vs;
 
   return function (tx) {
-    var sql = 'SELECT id, ' + fields.join (', ') 
+    var sql = 'SELECT id, ' + fields.join (', ')
             + ' FROM ' + table
             + (w == none ? '' : ' WHERE ' + w);
 
@@ -174,7 +191,11 @@ function db_query (table, fields, f, f_error, w, vs) {        //  {{{1
 //  Usage: db_insert (table, fields, records) -> none
 //
 //  Example:
-//    ...
+//    db (..., function () { return [
+//      ...
+//      DB.foo.iFoo ([{ k1: v1, k2: v2}, ...]),
+//      ...
+//    ]; });
 //
 
 function db_insert (table, fields, records) {                 //  {{{1
@@ -182,8 +203,8 @@ function db_insert (table, fields, records) {                 //  {{{1
     var qms = fields.map (function (x) { return '?'; }).join (', ');
 
     for (var i in records) {
-      var sql = 'INSERT INTO ' + table 
-              + ' ( id, ' + fields.join (', ') 
+      var sql = 'INSERT INTO ' + table
+              + ' ( id, ' + fields.join (', ')
               + ' ) VALUES ( NULL, ' + qms + ' )';
 
       if (DEBUG) {
@@ -204,7 +225,17 @@ function db_insert (table, fields, records) {                 //  {{{1
 //                    where_vals_key]) -> none
 //
 //  Example:
-//    ...
+//    db (..., function () { return [
+//      ...
+//      DB.foo.uFoo ([
+//      { k   : v,
+//        _W_ : 'x < ? AND y > ?',  // WHERE clause; use '?' !!!
+//        _V_ : [x, y]              // values for '?'s
+//      }, ...]),
+//      ...
+//    ]; });
+//
+//  TODO: WHERE ??? !!!
 //
 
 function db_update (table, fields, records, wk, vk) {         //  {{{1
@@ -220,8 +251,8 @@ function db_update (table, fields, records, wk, vk) {         //  {{{1
       var r_vals  = fs.map (function (x) { return records[i][x]; });
       var w_vals  = records[i][vk_] || [];
 
-      var sql     
-        = 'UPDATE ' + table + ' SET ' 
+      var sql
+        = 'UPDATE ' + table + ' SET '
         + fs.map (function (x) { return x + ' = ?' }).join (', ')
         + ' WHERE ' + records[i][wk_];
 

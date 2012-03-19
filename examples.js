6faebad4148d53365dae2eba2d8491cb402b6c6f
Felix C. Stegerman <felixstegerman@noxqslabs.nl>
2012-03-19 02:03:01 +0100

    ~ examples.js (better filter)
    Signed-off-by: Felix C. Stegerman <felixstegerman@noxqslabs.nl>

--- examples.js
+++ examples.js
@@ -32,7 +32,7 @@ db (dbName, dbDef, function (dbo) { return [
   // query records / callback; w/ filter
   dbo.qFoo (function (i, x) {
     console.log ('#=' + i, 'id=' + x.id, 'obj=', x);
-  }, none, db_and (db_ne ('id', 1), db_lt ('id', 7)) ),
+  }, none, db_and (db_ne ('id', 1), db_gt ('id', 4)) ),
 
 
   // function (tx) { ... }, ...
